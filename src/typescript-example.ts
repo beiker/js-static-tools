37a01d7586486c181e436395e09d1bb2e513aa8d
Oscar Alcántar Salazar <oscar.alcantars@gmail.com>
2019-12-07 12:07:53 -0600

    initial commit

---
+++ src/typescript-example.ts
@@ -0,0 +1,22 @@
+// function add(a: number, b: number) {
+//   return a + b;
+// }
+
+// interface User {
+//   name: {
+//     first: string;
+//     middle: string;
+//     last: string;
+//   };
+// }
+
+// function getFullname(user: User): string {
+//   const {
+//     name: {first, middle, last},
+//   } = user;
+//   return [first, middle, last].filter(Boolean).join('');
+// }
+
+// add(1, 2);
+
+// getFullname({name: {first: 'Joe', middle: 'Bud', last: 'Matthews'}});
